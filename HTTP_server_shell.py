e500e4b0862ce8e3422854aecde66e6562b7c00a
ariel <ariel@build-e.com>
2020-01-20 22:30:37 +0200

    default index.html for blank request url + remove chunks

--- HTTP_server_shell.py
+++ HTTP_server_shell.py
@@ -9,10 +9,12 @@ import socket
 # TO DO: set constants
 IP = '127.0.0.1'
 PORT = 80
-DEFAULT_URL = '\\'
+DEFAULT_URL = 'index.html'
 REDIRECTION_DICTIONARY = ['webroot\\redirection_file.html']
 FORBIDDEN_DIRECTORIES = ['webroot\\classified\\']
 SOCKET_TIMEOUT = 60000
+
+
 # REQUEST_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
 
@@ -21,40 +23,26 @@ def get_file_data(filename):
     print('Trying to open file ' + filename)
     file = open(filename, 'rb')
 
-    chunk = 0
-    data_length = 0
     with file as f:
-        #     chunk = f.read(1024)
-        #     file_data = (chunk,)
-        #     data_length += len(chunk)
-        #     while chunk:
-        #         print("Reading chunk")
-        chunk = f.read()
-        #         file_data += (chunk,)
-        data_length += len(chunk)
+        data = f.read()
 
-    return chunk, data_length
+    return data
 
 
 def handle_client_request(resource, client_socket):
     """ Check the required resource, generate proper HTTP response and send to client"""
     # TO DO : add code that given a resource (URL and parameters) generates the proper response
-    if resource == '':
+    if resource == '' or resource == '/':
         url = DEFAULT_URL
     else:
         url = resource
 
-    filetype = resource.split('.')[-1]
-    filename = resource
     optional_field = ''
 
     # TO DO: STATUS-CODE check if URL had been redirected, not available or other error code. For example:
     if url in REDIRECTION_DICTIONARY:
         status_code = '302 Found'
         optional_field = r'webroot\redirection_file.html\r\n'
-    elif url in FORBIDDEN_DIRECTORIES:
-        status_code = '403 Forbidden'
-        optional_field = str(len(FORBIDDEN_DIRECTORIES)) + '\r\n'
     else:
         status_code = '200 OK'
 
@@ -64,25 +52,31 @@ def handle_client_request(resource, client_socket):
         client_socket.send(http_header.encode())
     else:
         # TO DO: read the data from the file
-        data, data_length = get_file_data(filename)
+        file_name = 'webroot\\' + url.replace('/', '\\')  # Example: C:\Cyber\webroot\index.html
+        if os.path.isfile(file_name):
+            filetype = file_name.split('.')[-1]
+            data = get_file_data(file_name)
 
-        http_header += 'Content-Length: %d\r\n' % data_length
+            http_header += 'Content-Length: %d\r\n' % len(data)
 
-        if filetype == 'html':
-            http_header += 'Content-Type: %s\r\n' % 'text/html; charset=utf-8'
-        elif filetype == 'jpg' or filetype == 'ico':
-            http_header += 'Content-Type: %s\r\n' % 'image/jpeg'
-        elif filetype == 'js':
-            http_header += 'Content-Type: %s\r\n' % 'text/javascript; charset=UTF-8'
-        elif filetype == 'css':
-            http_header += 'Content-Type: %s\r\n' % 'text/css'
-        http_header += '\r\n'
-        client_socket.send(http_header.encode())
-        client_socket.send('\r\n'.encode())  # header and body should be separated by additional newline
-        # chunk = data[0]
-        client_socket.sendall(data)
-        # for chunk in data[1:]:
-        #     client_socket.sendall(chunk)
+            if filetype == 'html':
+                http_header += 'Content-Type: %s\r\n' % 'text/html; charset=utf-8'
+            elif filetype == 'jpg':
+                http_header += 'Content-Type: %s\r\n' % 'image/jpeg'
+            elif filetype == 'js':
+                http_header += 'Content-Type: %s\r\n' % 'text/javascript; charset=UTF-8'
+            elif filetype == 'css':
+                http_header += 'Content-Type: %s\r\n' % 'text/css'
+            else:
+                http_header += '\r\n'
+            http_header += '\r\n'
+            client_socket.send(http_header.encode())
+            client_socket.send('\r\n'.encode())  # header and body should be separated by additional newline
+            client_socket.send(data)
+        else:
+            print('Error 404 Not Found')
+            http_header = 'HTTP/1.1 404 Not Found\r\n\r\n'
+            client_socket.send(http_header.encode())
 
 
 def validate_http_request(request):
@@ -94,13 +88,10 @@ def validate_http_request(request):
     if not req[0] == 'GET':
         return False, '500 Internal Server Error'
 
-    final_request = 'webroot' + req[1].replace('/','\\') # Example: C:\Cyber\webroot\index.html
-    if not os.path.isfile(final_request):
-        return False, '404 Not Found'
     if not req[2] == 'HTTP/1.1':
         return False, '500 Internal Server Error'
 
-    return True, final_request
+    return True, req[1]
 
 
 def handle_client(client_socket):
@@ -114,11 +105,13 @@ def handle_client(client_socket):
         if valid_http:
             print('Got a valid HTTP request')
             handle_client_request(resource, client_socket)
+            break
         else:
             print(resource)
             break
     print('Closing connection')
     client_socket.close()
+
 
 def main():
     # Open a socket and loop forever while waiting for clients
